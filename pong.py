cc0902c0e6981c042f5608425457f38a04994eb9
Phillip Jones <ptj92e@gmail.com>
2020-04-23 10:00:23 -0500

    Adding movement to paddles

--- pong.py
+++ pong.py
@@ -36,7 +36,32 @@ ball.goto(0, 0)
 # Functions
 def paddle_a_up():
     y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard Binding
+#Tells program to listen for keyboard input
+wn.listen()
+#When the "W" key is pressed, the paddle_a_up funciton is called. It is case sensitive
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 #Main Game Loop
 while True:
